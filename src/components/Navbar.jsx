a075adb2fa3430db14b4ec24c2e6343a54af1dca
aruve <aruve28@gmail.com>
2024-04-06 18:43:34 -0400

    styled sign out button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,11 @@ import { Link } from "react-router-dom";
 import logo from "../../assets/logo-png.png";
 import { useSelector, useDispatch } from "react-redux";
 import {
-  signOutUserStart,deleteUserSuccess,deleteUserFailure
+  signOutUserStart,
+  deleteUserSuccess,
+  deleteUserFailure,
 } from "../redux/user/userSlice";
+
 export const Navbar = () => {
   const [menuDraw, setMenuDraw] = useState(false);
   const { currUser } = useSelector((state) => state.user_mod);
@@ -25,57 +28,12 @@ export const Navbar = () => {
     }
   };
 
-  // const [searchQuery, setSearchQuery] = useState("");
-  // const handleSearch = async () => {
-  //   console.log(searchQuery)
-
-  //   if (searchQuery.trim() !== "") {
-  //     try {
-  //       const response = await fetch(`/api/v1/cars/searchCars?query=${searchQuery.trim()}`, {
-  //         method: "GET",
-  //         headers: {
-  //           "Content-Type": "application/json",
-  //           Authorization: `Bearer ${currUser?.data?.token}`
-  //         }
-  //       });
-
-  //       if (!response.ok) {
-  //         throw new Error("Network response was not ok");
-  //       }
-
-  //       const data = await response.json();
-  //       setCars(data.data);
-  //     } catch (error) {
-  //       console.error("Error fetching search results:", error);
-  //       setError("Failed to fetch search results. Please try again later.");
-  //     } finally {
-  //       setIsLoading(false); // End loading
-  //     }
-  //   }
-  // };
-
   return (
     <nav className="navbar flex items-center justify-between px-10 py-4 bg-floralwhite">
       <Link to="/" className="text-2xl font-semibold">
         <img src={logo} alt="Logo" className="h-10 mr-4" />
         HPASS RENTALS
       </Link>
-      {/* <form className="flex items-center">
-        <input
-          type="text"
-          placeholder="Search items..."
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
-          className="border border-gray-400 py-2 px-4 rounded-l-lg focus:outline-none"
-        />
-        <button
-          type="button"
-          className="text-white bg-black py-2 px-4 rounded-r-lg"
-          onClick={handleSearch}
-        >
-          <FaSearch />
-        </button>
-      </form> */}
       <div className="md:hidden">
         <button
           className="block text-gray-800 hover:text-gray-900 focus:text-gray-900 focus:outline-none"
@@ -112,37 +70,37 @@ export const Navbar = () => {
       >
         <ul className="md:flex flex-col md:flex-row md:ml-auto md:mr-0 items-center">
           <li>
-            <a href="/" className="md:ml-4 text-lg font-bold">
-              Home
-            </a>
+            <button className="md:ml-4 text-lg font-bold text-black bg-transparent border border-black rounded-md px-4 py-2 transition-colors hover:bg-black hover:text-white">
+              <Link to="/sign-up">Home</Link>
+            </button>
           </li>
           {currUser?.data?.username === "admin" && (
             <li>
-              <Link to="/modify-listing" className="md:ml-4 text-lg font-bold">
-                Admin Page
-              </Link>
+              <button className="md:ml-4 text-lg font-bold text-black bg-transparent border border-black rounded-md px-4 py-2 transition-colors hover:bg-black hover:text-white">
+                <Link to="/sign-up">Admin Page</Link>
+              </button>
             </li>
           )}
           <li>
-            <Link to="/Listingpage" className="md:ml-4 text-lg font-bold">
-              View Listings
-            </Link>
+            <button className="md:ml-4 text-lg font-bold text-black bg-transparent border border-black rounded-md px-4 py-2 transition-colors hover:bg-black hover:text-white">
+              <Link to="/sign-up">Browse</Link>
+            </button>
           </li>
-          {currUser?.data?.username?  (
-             <li>
-             <button
-               onClick={handleSignOut}
-               className="md:ml-4 text-lg font-bold"
-             >
-               Sign Out
-             </button>
-           </li>
+          {currUser?.data?.username ? (
+            <li>
+            <button
+              onClick={handleSignOut}
+              className= "md:ml-4 text-lg font-bold text-black  bg-red-500 border border-black rounded-md px-4 py-2 transition-colors hover:bg-black hover:text-white"
+            >
+              Sign Out
+            </button>
+          </li>
           ) : (
             <li>
-            <Link to="/sign-up" className="md:ml-4 text-lg font-bold">
-              Register
-            </Link>
-          </li>
+              <button className="md:ml-4 text-lg font-bold text-black bg-transparent border border-black rounded-md px-4 py-2 transition-colors hover:bg-black hover:text-white">
+                <Link to="/sign-up">Register</Link>
+              </button>
+            </li>
           )}
         </ul>
       </div>
