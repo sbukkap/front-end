5ed5d6f76efedbfb981e51987bc92bb083f2e354
SnehaManohar23 <snemanoh@iu.edu>
2024-04-10 19:58:04 -0400

    ratings and reviews

--- src/pages/CarDetailsPage.jsx
+++ src/pages/CarDetailsPage.jsx
@@ -1,15 +1,38 @@
 import React, { useEffect, useState } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import { useSelector } from "react-redux";
+import { Rating, Typography, Box, Grid, Paper } from '@mui/material';
 
 
 function CarDetailsPage() {
   const { currUser } = useSelector((state) => state.user_mod);
   const { carId } = useParams();
   const [carDetails, setCarDetails] = useState(null);
+  const [reviews, setReviews] = useState([]);
+  const [reviewFormData, setReviewFormData] = useState({ rating: '', review: '' });
+  const [rating, setRating] = useState(0);
   const [selectedImage, setSelectedImage] = useState('');
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
+
+
+  const fetchReviews = async () => {
+    try {
+      const response = await fetch(`/api/v1/reviews/getallCarReviews/${carId}`, {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${currUser?.data?.token}`
+        }
+      });
+      if (!response.ok) {
+        throw new Error('Could not fetch reviews');
+      }
+      const reviewData = await response.json();
+      setReviews(reviewData.data);
+    } catch (error) {
+      setError(error.message);
+    }
+  };
 
   useEffect(() => {
     const fetchCarDetails = async () => {
@@ -27,6 +50,7 @@ function CarDetailsPage() {
         const data = await response.json();
         setCarDetails(data.data);
         setSelectedImage(data.data.image_url[0]); 
+        fetchReviews();
       } catch (error) {
         setError(error.message);
       } finally {
@@ -34,8 +58,48 @@ function CarDetailsPage() {
       }
     };
 
+    
     fetchCarDetails();
   }, [carId, currUser?.data?.token]);
+
+  const postReview = async (reviewData) => {
+    try {
+      const response = await fetch(`/api/v1/reviews/createReview`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${currUser?.data?.token}`
+        },
+        body: JSON.stringify(reviewData)
+      });
+      if (!response.ok) {
+        throw new Error('Failed to post review');
+      }
+      fetchReviews();
+    } catch (error) {
+      setError(error.message);
+    }
+};
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setReviewFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmitReview = async (e) => {
+    e.preventDefault();
+    const dataToSubmit = {
+      ratings: rating, 
+      review: reviewFormData.review,
+      item_id: carId,
+    };
+
+    await postReview(dataToSubmit);
+    setReviewFormData({ rating: '', review: '' });
+  };
 
   if (isLoading) {
     return <div className="text-center text-xl mt-10">Loading...</div>;
@@ -49,60 +113,127 @@ function CarDetailsPage() {
     return <div className="text-center text-xl text-gray-500 mt-10">Car not found</div>;
   }
 
+  const mapLink = `/itemLocation?lat=${carDetails.location.lat}&lng=${carDetails.location.lng}`;
+  const backLink = "/Listingpage";
+
   return (
-    <div className="bg-customecolor-50 min-h-screen pt-10">
-      <div className="container mx-auto px-4">
-        <div className="bg-white shadow-sm rounded-lg overflow-hidden">
-          {/* Image Gallery */}
-          <div className="text-center">
-          <h1 className="text-4xl md:text-4xl font-bold mb-4 font-mono">{carDetails.carMake} {carDetails.carModel}</h1>
-            <img 
-              className="w-full max-w-3xl mx-auto h-auto object-contain"
-              src={selectedImage}
-              alt={`${carDetails.carMake} ${carDetails.carModel}`} 
-            />
-          </div>
-          {/* Thumbnails */}
-          <div className="flex justify-center space-x-1 overflow-x-auto py-2">
-            {carDetails.image_url.map((image, index) => (
-              <img
-                key={index}
-                onClick={() => setSelectedImage(image)}
-                className={`object-cover rounded-lg cursor-pointer p-1 border-2 border-transparent hover:border-blue-500 ${selectedImage === image ? 'border-blue-500' : ''}`}
-                src={image}
-                alt={`Thumbnail ${index}`}
-                style={{ width: '100px', height: '75px' }} 
-              />
-            ))}
-          </div>
-          {/* Car Details */}
-          <div className="p-6 bg-floralwhite">
-          {/* <h1 className="text-3xl font-bold mb-4">{carDetails.carMake} {carDetails.carModel}</h1> */}
-            <p className="text-lg "><strong>Year:</strong> {carDetails.year}</p>
-            <p className="text-lg "><strong>Mileage:</strong> {carDetails.mileage} miles</p>
-            <p className="text-lg "><strong>Transmission:</strong> {carDetails.transmission}</p>
-            <p className="text-lg "><strong>Fuel Type:</strong> {carDetails.fuelType}</p>
-            <p className="text-lg "><strong>Seats:</strong> {carDetails.seats}</p>
-            <p className="text-lg "><strong>Price Per Day:</strong> ${carDetails.pricePerDay}</p>
-            <p className="text-lg "><strong>Available From:</strong> {new Date(carDetails.availableFrom).toLocaleDateString()}</p>
-            <p className="text-lg "><strong>Available To:</strong> {new Date(carDetails.availableTo).toLocaleDateString()}</p>
-            <Link to={`/itemLocation?lat=${carDetails.location.lat}&lng=${carDetails.location.lng}`} className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-6 rounded-full transition ease-in-out duration-300"
-              >
-          View item location
-        </Link>
-          
-              <Link 
-                to="/Listingpage"
-                className="text-center mt-6 inline-block bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded-full transition ease-in-out duration-300"
-              >
-                Go back
-              </Link>
+    <div className="min-h-screen bg-customcolor-100 py-10">
+      <div className="container mx-auto px-4 max-w-7xl">
+      <div className="flex justify-between mb-6">
+          <Link to={backLink} className="text-blue-600 hover:text-blue-700 py-2 px-8 transition duration-300">
+            ← Go back
+          </Link>
+          <Link to={mapLink} className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-8 rounded-full transition ease-in-out duration-300">
+            View on Map
+          </Link>
+        </div>
+        <div className="md:flex md:-mx-4">
+  
+          {/* Column for Car Image Gallery and Reviews */}
+          <div className="md:w-2/3 px-4 mb-6">
+            <div className="bg-white shadow rounded-lg mb-6">
+              {/* Car Image */}
+              <div className="text-center p-4 ">
+                <img
+                  className="inline-block mx-auto h-auto max-w-full rounded-lg"
+                  src={selectedImage}
+                  alt={`${carDetails.carMake} ${carDetails.carModel}`}
+                />
+                {/* Thumbnails */}
+                <div className="flex justify-center space-x-1 overflow-x-auto py-2">
+                  {carDetails.image_url.map((image, index) => (
+                    <img
+                      key={index}
+                      onClick={() => setSelectedImage(image)}
+                      className="inline-block object-cover rounded-lg cursor-pointer p-1 border-2 border-transparent hover:border-blue-500"
+                      src={image}
+                      alt={`Thumbnail ${index}`}
+                      style={{ width: '100px', height: '75px' }}
+                    />
+                  ))}
+                </div>
+              </div>
+            </div>
+  
+            {/* Reviews Section */}
+            <div className="bg-white shadow rounded-lg p-4">
+              <h6 className="text-lg font-semibold mb-4">Reviews</h6>
+              {reviews.length > 0 ? (
+                reviews.map((review) => (
+                  <div key={review._id} className="border-b border-gray-200 mb-4 pb-4">
+                    <Rating name="read-only" value={review.ratings} readOnly />
+                    <p className="text-gray-800 mt-2">{review.review}</p>
+                  </div>
+                ))
+              ) : (
+                <p className="text-gray-500">No reviews yet.</p>
+              )}
             </div>
           </div>
-        </div>
-      </div>
   
+          {/* Right Column for Car Details and Review Form */}
+          <div className="md:w-1/3 px-4">
+            {/* Car Details */}
+            <div className="bg-white shadow rounded-lg p-4 mb-6">
+              <h5 className="text-xl font-semibold mb-2">{carDetails.carMake} {carDetails.carModel}</h5>
+              <dl>
+              <p><strong>Year:</strong> {carDetails.year}</p>
+              <p><strong>Mileage:</strong> {carDetails.mileage}</p>
+              <p><strong>Transmission:</strong> {carDetails.transmission}</p>
+              <p><strong>Fuel Type:</strong> {carDetails.fuelType}</p>
+              <p><strong>Seats:</strong> {carDetails.seats}</p>
+              <p><strong>Price Per Day:</strong> {carDetails.pricePerDay}</p>
+              <p><strong>Available From:</strong> {new Date(carDetails.availableFrom).toLocaleDateString()}</p>
+              <p><strong>Available To:</strong> {new Date(carDetails.availableTo).toLocaleDateString()}</p>
+          
+              </dl>
+            </div>
+  
+            {/* Review Form */}
+            <div className="bg-white shadow rounded-lg p-4">
+              <h6 className="text-lg font-semibold mb-4">Submit a Review</h6>
+              <form onSubmit={handleSubmitReview}>
+                <div className="mb-4">
+                  <Rating
+                    name="simple-controlled"
+                    value={rating}
+                    onChange={(event, newValue) => {
+                      setRating(newValue);
+                      setReviewFormData({ ...reviewFormData, rating: newValue });
+                    }}
+                  />
+                </div>
+                <div className="mb-4">
+                  <textarea
+                    id="review"
+                    name="review"
+                    placeholder="Write your review here"
+                    value={reviewFormData.review}
+                    onChange={handleInputChange}
+                    required
+                    className="w-full border border-gray-300 rounded p-2"
+                    rows="4"
+                  ></textarea>
+                </div>
+                <div className="text-right">
+                  <button
+                    type="submit"
+                    className="rounded bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 transition-colors duration-300"
+                  >
+                    Submit Review
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+                    
+          
+      </div>
+    </div>
+    </div>
   );
 }
-
-export default CarDetailsPage;
+    
+  
+  export default CarDetailsPage;
+  
