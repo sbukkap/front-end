08959d6029bc923347604e8595b4cec12ae65806
aruve <aruve28@gmail.com>
2024-04-17 13:36:36 -0400

    Status Page- Payment

--- src/components/Payment/PaymentFailure.jsx
+++ src/components/Payment/PaymentFailure.jsx
@@ -2,7 +2,18 @@ import React from 'react';
 
 const PaymentFailure = () => {
     return (
-        <div style={{ textAlign: 'center', marginTop: '50px' }}>
+        <div style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            backgroundColor: '#fff', // Set a background color
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center'
+        }}>
             <div style={{ color: 'red', fontSize: '100px', marginBottom: '20px' }}>&#10060;</div>
             <h1 style={{ color: 'red' }}>Payment Failed</h1>
             <p>Oops! Something went wrong with your payment.</p>
--- src/components/Payment/PaymentSuccess.jsx
+++ src/components/Payment/PaymentSuccess.jsx
@@ -2,10 +2,21 @@ import React from 'react';
 
 const PaymentSuccess = () => {
     return (
-        <div style={{ textAlign: 'center', marginTop: '50px' }}>
+        <div style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            backgroundColor: '#fff', // Set a background color
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center'
+        }}>
             <div style={{ color: 'green', fontSize: '100px', marginBottom: '20px' }}>&#10004;</div>
             <h1 style={{ color: 'green' }}>Payment Successful</h1>
-            
+            <p>Thank you for your purchase!</p>
         </div>
     );
 };
